77f8d78d8a6e9697f9cc9b902649d93de2a6e356
Maksym Shokin <maksym.sh@upplabs.com>
2020-11-22 19:49:50 +0200

    button-stories

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './button.module.css'
+
+const Button = ({ variant = 'Primary', children, ...rest }) => {
+  return (
+    <button className={`${classes.button} ${classes[variant]}`} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
